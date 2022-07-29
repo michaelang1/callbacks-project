180864c20526a1071fe7e1085b585c72fa1d5ccc
Michael An <huayu.michael@gmail.com>
2022-07-29 16:51:34 -0700

    10-my-every.js is done

--- problems/10-my-every.js
+++ problems/10-my-every.js
@@ -21,12 +21,54 @@ console.log(myEvery(['book', 'door'], hasO));           // true
 console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 *******************************************************************************/
 
-let myEvery = function() {
+let myEvery = function (array, cb) {
+    // method 1: use for loop and a counter;
+
+    // for (let i = 0; i < array.length; i++) {
+    //     let el = array[i];
+    //     if (cb(el, i) === false) {
+    //         return false;
+    //     }
+    // }
+
+    // return true;
+
+    // method 2: use forEach method and
+    // a counter, the above returns do not work for forEach
+    // as forEach always returns undefined after the loop,
+    // have to use a counter;
+
+    let countTrue = 0;
+
+    array.forEach(function(el) {
+        if (cb(el)) {
+            countTrue++;
+        }
+    });
+
+    if (countTrue === array.length) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
 
 
+
+let isEven = function (num) {
+    return num % 2 === 0;
+};
+
+let hasO = function (string) {
+    return string.includes('o');
+};
+
+console.log(myEvery([4, 8, 6, 10], isEven));            // true
+console.log(myEvery([4, 7, 6, 10], isEven));            // false
+console.log(myEvery(['book', 'door'], hasO));           // true
+console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 
 
 
